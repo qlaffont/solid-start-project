fd9ae5fd4d1e7b24e71485f8d31bedd4627874fa
Quentin <qlaffont13@gmail.com>
2023-12-20 12:08:58 +0100

    add example for imagemin

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,31 @@
 import solid from "solid-start/vite";
 import {defineConfig} from "vite";
 import DynamicPublicDirectory from 'vite-multiple-assets';
+import imagemin from 'unplugin-imagemin/vite';
 
 export default defineConfig({
   ssr: {
     external: ["solid-start-cmp"]
   },
   plugins: [solid(),
-    DynamicPublicDirectory(['node_modules/solid-start-cmp/public','public']),],
+    DynamicPublicDirectory(['node_modules/solid-start-cmp/public','public']),
+    imagemin({
+      mode: 'sharp',
+      beforeBundle: false,
+      // Default configuration options for compressing different pictures
+      compress: {
+        jpg: {
+          quality: 50,
+        },
+        jpeg: {
+          quality: 70,
+        },
+        png: {
+          quality: 70,
+        },
+        webp: {
+          quality: 70,
+        },
+      },
+    }),],
 });
